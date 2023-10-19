390f4809cdbf8b0377955b3721195fcea61e825e
Vitor Cesario <vitorsousa676@gmail.com>
2023-10-19 15:29:50 -0300

    Introdução e pratica de javascript

---
+++ assets/script/arquivo.js
@@ -0,0 +1,34 @@
+let valor = 3
+
+let vaolor2 = "3"
+
+let nome = "Vitor Cesario"
+
+const url ="http://google.com.br"
+
+
+console.log(valor)
+console.log(typeof(valor))
+
+console.log(typeof(vaolor2))
+
+console.log(url)
+
+function ola(){
+    console.log("Olá Mundo")
+}
+ola()
+
+function soma(nuemro1,nuemro2){
+    return nuemro1+nuemro2
+}
+console.log(soma(3,5))
+
+//arrow function
+const olaMundo = () => console.log("Olá Mundo 2")
+
+olaMundo()
+
+const somaNum = (num1,num2) => num1+num2
+
+console.log(somaNum(22,88))
